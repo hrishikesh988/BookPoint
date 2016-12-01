0bc999961996de6aa253c860712bcdab628e3b55
hrishikesh988 <hndeshmukh89@gmail.com>
2016-12-01 03:33:16 -0800

    SignUP Activity Layout Added

--- BookPoint/app/src/main/java/com/deshmukh/hrishikesh/bookpoint/SignUpActivity.java
+++ BookPoint/app/src/main/java/com/deshmukh/hrishikesh/bookpoint/SignUpActivity.java
@@ -2,10 +2,9 @@ package com.deshmukh.hrishikesh.bookpoint;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +12,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import static android.R.attr.data;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -25,12 +27,14 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText mConfirmPassword;
     private EditText mLocation;
 
+    private ImageView mProfilePic;
     private Button mRegisterButton;
     private Button mResetButton;
-    private ImageView mProfilePic;
     private ImageButton mCameraButton;
+    private ImageButton mLocationButton;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int PLACE_PICKER_REQUEST=2;
 
 
     @Override
@@ -49,6 +53,7 @@ public class SignUpActivity extends AppCompatActivity {
         mResetButton = (Button) findViewById(R.id.reset_button);
         mProfilePic = (ImageView) findViewById(R.id.profile_pic_iv);
         mCameraButton = (ImageButton) findViewById(R.id.camera_button);
+        mLocationButton = (ImageButton) findViewById(R.id.location_button);
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,6 +95,27 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        mLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                Intent intent;
+                try{
+                    intent = builder.build(SignUpActivity.this);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                }
+
+                catch (GooglePlayServicesNotAvailableException e){
+                    e.printStackTrace();
+                }
+                catch (GooglePlayServicesRepairableException e){
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
 
     }
 
@@ -99,6 +125,12 @@ public class SignUpActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mProfilePic.setImageBitmap(imageBitmap);
+        }
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, SignUpActivity.this);
+                mLocation.setText(place.getAddress());
+            }
         }
     }
 }
