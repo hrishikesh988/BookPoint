4cca8c662bbacfdb85b633c5ddf21c1b4acdd39c
dhawaldabholkar <dhawaldabholkar94@gmail.com>
2016-12-06 22:39:29 -0800

    BuyBook and Sell Book Activity

--- app/src/main/java/com/deshmukh/hrishikesh/bookpoint/BuyAndSellActivity.java
+++ app/src/main/java/com/deshmukh/hrishikesh/bookpoint/BuyAndSellActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ public class BuyAndSellActivity extends AppCompatActivity {
 
     private TextView mUserName_TV;
     private ImageView mUserPic;
+    private Button mbuybooksbutton;
+    private Button msellbooksbutton;
     public static Intent newIntent(Context packageContext, Bitmap image, String username) {
         Intent i = new Intent( packageContext, BuyAndSellActivity.class);
         i.putExtra("image", image);
@@ -27,6 +31,9 @@ public class BuyAndSellActivity extends AppCompatActivity {
         mUserName_TV = (TextView) findViewById(R.id.username_tv);
         mUserPic = (ImageView) findViewById(R.id.user_imageView);
 
+        mbuybooksbutton = (Button) findViewById(R.id.buy_button);
+        msellbooksbutton = (Button) findViewById(R.id.sell_button);
+
         Bundle extras = getIntent().getExtras();
         Bitmap imageBitmap = (Bitmap) extras.get("image");
         String username = extras.getString("user_name");
@@ -37,6 +44,31 @@ public class BuyAndSellActivity extends AppCompatActivity {
 
             mUserPic.setImageDrawable(getResources().getDrawable(R.drawable.profile_pic, null));
         }
+
+
+
+        msellbooksbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(LoginActivity.this, "Sign up code goes here", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(BuyAndSellActivity.this, SellActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        mbuybooksbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(LoginActivity.this, "Sign up code goes here", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(BuyAndSellActivity.this, BuyActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
     }
     @Override
     public void onBackPressed() {
